33e7952aabf10ea968259447df58ebfbf5688487
yamen1995 <funmoon7@gmail.com>
2025-07-09 07:43:08 +0300

    feature : added books listing and count to author views

--- library_management/models/author.py
+++ library_management/models/author.py
@@ -6,3 +6,20 @@ class LibraryAuthor(models.Model):
 
     name = fields.Char(string='Name', required=True)
     book_ids = fields.One2many('library.book', 'author_id', string='Books')
+    book_count = fields.Integer(string="Book Count", compute="_compute_book_count")
+
+    def _compute_book_count(self):
+        for author in self:
+            author.book_count = len(author.book_ids)
+    def action_view_books(self):
+        """ Action to view books by this author """
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Books by Author: %s' % self.name,
+            'res_model': 'library.author',
+            'res_id': self.id,
+            'view_mode': 'form',
+            'view_id': self.env.ref('library_management.view_library_author_form_tree').id,
+            'target': 'new',
+        }
