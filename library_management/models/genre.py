68ed846c8ae0e7d92b9ac38ec37371e409b34467
yamen1995 <funmoon7@gmail.com>
2025-07-09 06:59:23 +0300

    feature : added book count and book listing to genre views

--- library_management/models/genre.py
+++ library_management/models/genre.py
@@ -9,3 +9,20 @@ class LibraryGenre(models.Model):
     _sql_constraints = [
         ('unique_genre_name', 'unique(name)', 'Genre name must be unique!')
     ]
+    book_count = fields.Integer(string="Book Count", compute="_compute_book_count")
+
+    def _compute_book_count(self):
+        for genre in self:
+            genre.book_count = len(genre.book_ids)
+    def action_view_books(self):
+        """ Action to view books in this genre """
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Books in Genre: %s' % self.name,
+            'res_model': 'library.genre',
+            'res_id': self.id,
+            'view_mode': 'form',
+            'view_id': self.env.ref('library_management.view_library_genre_form_tree').id,
+            'target': 'new',
+        }
